a44d019df77d997db3e0774d3eb60183895766e8
lbedoyas <lucho.bedoya1@hotmail.com>
2018-10-20 07:54:31 -0500

    Adicion de ejercicios de clase 1 Python
    Adicion de ejercicios de clase 1 Python

---
+++ Clase 1/matematicas2.py
@@ -0,0 +1,7 @@
+from math import sqrt as raiz, pow as exponente
+
+dato=int(input("Ingrese el valor entero:   "))
+raiz_cuadrada=raiz(dato)
+cubo=exponente(dato,3)
+print("La raiz raiz_cuadrada es:   ",raiz_cuadrada)
+print("El cubo es:   ",cubo)
