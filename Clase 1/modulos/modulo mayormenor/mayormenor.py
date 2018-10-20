51568cc1b23184cbe66e1c327843427c11e2225b
lbedoyas <lucho.bedoya1@hotmail.com>
2018-10-20 09:15:14 -0500

    clases and modulos
    clases and modulos

---
+++ Clase 1/modulos/modulo mayormenor/mayormenor.py
@@ -0,0 +1,13 @@
+def numero_mayor(x1,x2):
+	if x1>x2:
+		return x1
+	else:
+		return x2
+
+
+def numero_menor(x1,x2):
+	if x1<x2:
+		return x1
+	else:
+		return x2
+
---
+++ Clase 1/modulos/modulo mayormenor/principal1.py
@@ -0,0 +1,7 @@
+from mayormenor import numero_menor
+
+n1=int(input("Ingrese primer numero:   "))
+n2=int(input("Ingrese segundo numero:  "))
+print("El menor de los 2 valores es",numero_menor(n1,n2))
+
+
